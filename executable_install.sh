9d297931bafd81ecbac713173da84687cb270dff
meeplabsdev <76081175+meeplabsdev@users.noreply.github.com>
2025-02-15 18:46:16 +0000

    Update install.sh

--- executable_install.sh
+++ executable_install.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Add "six" user
+USER_NAME="six"
+if ! id -u "$USER_NAME" > /dev/null 2>&1; then
+    useradd -m -s /bin/bash "$USER_NAME"
+fi
+
 # Install yay
 if ! command -v yay > /dev/null; then
     sudo pacman -S --needed base-devel
