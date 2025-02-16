c1ae6580a7775746ee2106afe273a8bd5a2e617a
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-02-16 20:49:53 +0000

    Update install.sh

--- executable_install.sh
+++ executable_install.sh
@@ -43,4 +43,4 @@ fi
 
 # Install once-and-done software
 sudo pacman -S --needed --noconfirm kitty pipewire wireplumber wayland qt5-wayland qt6-wayland xorg-server xorg-server-common noto-fonts llvm llvm-libs meson wayland-protocols pcre2 json-c pango cairo gdk-pixbuf2 swaybg nvidia nvidia-dkms nvidia-utils vulkan-nouveau libdrm pixman polkit xdg-desktop-portal-wlr
-yay -S --needed --noconfirm downgrade swayfx wlroots libva-nvidia-driver swaylock-git swayidle-git
+yay -S --needed --noconfirm downgrade swayfx wlroots libva-nvidia-driver swaylock-git swayidle-git clipman
