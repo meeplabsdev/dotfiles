95cb970f5206b3614f58c57a965ef5aff50f1fd0
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-02-16 15:41:56 +0000

    Update install.sh

--- executable_install.sh
+++ executable_install.sh
@@ -51,7 +51,7 @@ if ! pacman -Q mesa | grep -q "1:24.2.7-1"; then
 fi
 
 # Install llvm
-sudo pacman -S --noconfirm llvm18 llvm18-libs
+sudo pacman -S --needed --noconfirm llvm18 llvm18-libs
 if ! pacman -Q llvm | grep -q "18.1.8-5"; then
     curl -O "https://archive.archlinux.org/packages/l/llvm/llvm-18.1.8-5-x86_64.pkg.tar.zst"
     sudo pacman -U --noconfirm "llvm-18.1.8-5-x86_64.pkg.tar.zst"
@@ -62,8 +62,10 @@ fi
 
 # Install once-and-done software
 sudo pacman -S --needed --noconfirm kitty mako pipewire wireplumber xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland xorg-server xorg-server-common noto-fonts
+yay -S --needed --noconfirm downgrade
 
 # Install hyprland
 if ! command -v hyprctl &> /dev/null; then
-    sudo pacman -S --noconfirm hyprland nvidia-dkms nvidia-utils egl-wayland libva-nvidia-driver
+    sudo pacman -S --noconfirm hyprland nvidia nvidia-utils
+    sudo downgrade hyprland nvidia nvidia-utils
 fi
