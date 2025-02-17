5b24c68b7979c0b3f2b13e6042c992fd69bcdc67
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-02-17 01:12:19 +0000

    Update install.sh

--- executable_install.sh
+++ executable_install.sh
@@ -45,5 +45,6 @@ fi
 sudo pacman -S --needed --noconfirm kitty pipewire wireplumber wayland qt5-wayland qt6-wayland xorg-server xorg-server-common noto-fonts llvm llvm-libs meson wayland-protocols pcre2 json-c pango cairo gdk-pixbuf2 swaybg nvidia nvidia-dkms nvidia-utils vulkan-nouveau libdrm pixman polkit xdg-desktop-portal-wlr pipewire-jack waybar mpv uwsm
 yay -S --needed --noconfirm downgrade swayfx wlroots libva-nvidia-driver swaylock-git swayidle-git clipman sway-screenshot rustup persway wdisplays-git swayosd-git wlogout walker
 
-# Set up sway/uwsm
+# Finalise
 sudo systemctl set-default graphical.target
+sudo systemctl enable swayosd-libinput-backend.service
