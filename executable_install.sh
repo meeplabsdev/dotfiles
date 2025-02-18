5596170105f561c4191102c937a6b72ab0420dc1
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-02-18 12:06:02 +0000

    Update install.sh

--- executable_install.sh
+++ executable_install.sh
@@ -42,8 +42,8 @@ if ! command -v yay &> /dev/null; then
 fi
 
 # Install once-and-done software
-sudo pacman -S --needed --noconfirm alacritty pipewire wireplumber wayland qt5-wayland qt6-wayland xorg-server xorg-server-common noto-fonts llvm llvm-libs meson wayland-protocols pcre2 json-c pango cairo gdk-pixbuf2 swaybg nvidia nvidia-dkms nvidia-utils vulkan-nouveau libdrm pixman polkit xdg-desktop-portal-wlr pipewire-jack waybar mpv uwsm otf-font-awesome python-pywal swaync code 
-yay -S --needed --noconfirm downgrade sway-nvidia wlroots libva-nvidia-driver swaylock-git swayidle-git clipman sway-screenshot rustup persway wdisplays-git swayosd-git wlogout walker firefox coppwr-bin python-pywalfox hyprpicker nerd-fonts-git cohesion-git notion-calendar-electron
+sudo pacman -S --needed --noconfirm alacritty pipewire wireplumber wayland qt5-wayland qt6-wayland xorg-server xorg-server-common noto-fonts llvm llvm-libs meson wayland-protocols pcre2 json-c pango cairo gdk-pixbuf2 swaybg nvidia nvidia-dkms nvidia-utils vulkan-nouveau libdrm pixman polkit xdg-desktop-portal-wlr pipewire-jack waybar mpv uwsm otf-font-awesome python-pywal swaync code doublecmd-qt6
+yay -S --needed --noconfirm downgrade sway-nvidia wlroots libva-nvidia-driver swaylock-git swayidle-git clipman sway-screenshot rustup persway wdisplays-git swayosd-git wlogout walker firefox coppwr-bin python-pywalfox hyprpicker nerd-fonts-git cohesion-git notion-calendar-electron nwg-look qogir-icon-theme materia-gtk-theme illogical-impulse-bibata-modern-classic-bin
 
 # Finalise
 sudo systemctl set-default graphical.target
